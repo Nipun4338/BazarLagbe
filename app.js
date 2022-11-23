da272e32dae535073bca33b40c0407e21cf94528
Nipun Paul <49658560+Nipun4338@users.noreply.github.com>
2022-11-24 02:32:00 +0600

    first commit

--- app.js
+++ app.js
@@ -135,11 +135,11 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-let port =process.env.PORT;
+let port=process.env.PORT;
 if(port==null || port==""){
     port=3000;
 }
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server started!");
 });
